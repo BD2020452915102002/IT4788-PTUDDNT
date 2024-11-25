3dbb57697f16258908a880fc738c5a5f7aefc756
ducbd <duc.bd204529@sis.hust.edu.vn>
2024-11-25 20:04:43 +0700

    update ui

---
+++ lib/presentation/screens/lecturer/class/assignment/grade_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class GradeScreen extends StatefulWidget {
+  const GradeScreen({super.key});
+
+  @override
+  State<GradeScreen> createState() => _GradeScreenState();
+}
+
+class _GradeScreenState extends State<GradeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/presentation/screens/student/class/detail_class_screen_student.dart
+++ lib/presentation/screens/student/class/detail_class_screen_student.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:ptuddnt/core/constants/colors.dart';
 import 'package:ptuddnt/core/utils/token.dart';
-import 'about_material/list_material.dart';
+import 'package:ptuddnt/presentation/screens/student/class/about_material/list_material.dart';
 
 class DetailClassScreenStudent extends StatelessWidget {
   final Map<dynamic, dynamic> classData;
+
   const DetailClassScreenStudent({super.key, required this.classData});
 
   @override
@@ -37,7 +38,6 @@ class DetailClassScreenStudent extends StatelessWidget {
           children: [
             _ClassInfoButton(classId: classData['class_id']),
             _ViewMaterialsButton(classData: classData),
-            // _AssignmentsButton(classData: classData),
             _AttendanceButton(classData: classData),
             _RequestLeaveButton(classId: classData['class_id']),
           ],
@@ -49,7 +49,9 @@ class DetailClassScreenStudent extends StatelessWidget {
 
 class _ClassInfoButton extends StatelessWidget {
   final String classId;
+
   const _ClassInfoButton({required this.classId});
+
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
@@ -67,34 +69,37 @@ class _ClassInfoButton extends StatelessWidget {
 }
 
 class _ViewMaterialsButton extends StatefulWidget {
-
   final Map<dynamic, dynamic> classData;
+
   const _ViewMaterialsButton({required this.classData});
 
   @override
   _ViewMaterialsButtonState createState() => _ViewMaterialsButtonState();
 }
+
 class _ViewMaterialsButtonState extends State<_ViewMaterialsButton> {
-   late final dynamic classId;
+  late final dynamic classId;
   String token = '';
+
   @override
   void initState() {
     super.initState();
     classId = widget.classData['class_id'];
     _loadToken();
   }
+
   Future<void> _loadToken() async {
     setState(() {
       token = Token().get() ?? 'Token not found';
     });
-    print("Class ID: ${classId}");
+    print("Class ID: $classId");
     print("Token: $token");
   }
+
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: () {
-        // Logic khi nhấn vào "Xem tài liệu môn học"
         print("classId: $classId");
         print('token: $token');
         Navigator.push(
@@ -103,31 +108,9 @@ class _ViewMaterialsButtonState extends State<_ViewMaterialsButton> {
             builder: (context) => ListMaterialScreen(token: token, classId: classId),
           ),
         );
-
       },
       style: _buttonStyle,
       child: _getButtonContent(Icons.share, 'Xem tài liệu môn học'),
-    );
-  }
-}
-
-class _AssignmentsButton extends StatelessWidget {
-  final Map<dynamic, dynamic> classData;
-
-  const _AssignmentsButton({required this.classData});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.pushNamed(
-          context,
-          '/list-assignment-student',
-          arguments: classData,
-        );
-      },
-      style: _buttonStyle,
-      child: _getButtonContent(Icons.grade, 'Danh sách bài tập/ kiểm tra'),
     );
   }
 }
@@ -136,13 +119,13 @@ class _AttendanceButton extends StatelessWidget {
   final Map<dynamic, dynamic> classData;
 
   const _AttendanceButton({required this.classData});
+
   @override
   Widget build(BuildContext context) {
     final classId = classData['class_id'] as String;
 
     return ElevatedButton(
       onPressed: () {
-        // Logic khi nhấn vào "Điểm danh"
         Navigator.pushNamed(context, '/absent-record-student', arguments: classId);
       },
       style: _buttonStyle,
@@ -173,30 +156,27 @@ class _RequestLeaveButton extends StatelessWidget {
 }
 
 Widget _getButtonContent(IconData icon, String text) {
-  return Expanded(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: AppColors.primary, size: 30),
-        const SizedBox(height: 8),
-        Flexible(
-          child: Text(
-            text,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-            textAlign: TextAlign.center,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(icon, color: AppColors.primary, size: 30),
+      const SizedBox(height: 8),
+      Flexible(
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
           ),
+          textAlign: TextAlign.center,
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
         ),
-      ],
-    ),
+      ),
+    ],
   );
 }
-
 
 final _buttonStyle = ElevatedButton.styleFrom(
   backgroundColor: Colors.yellow[50],
