63ac1fd2610c6fdcf79c253ee64d9e51de347089
duc.bd <daonc@carpla.vn>
2024-11-27 17:17:54 +0700

    update

--- lib/presentation/screens/common/chat.dart
+++ lib/presentation/screens/common/chat.dart
@@ -1,10 +1,64 @@
-import 'package:flutter/cupertino.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
 
   @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Received message: ${message.notification?.title}, ${message.notification?.body}');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('${message.notification?.title}: ${message.notification?.body}')),
+      );
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Text('aaaaaaaaaaaaa');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (ctx, index) => ListTile(
+                title: Text('Message $index'),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      labelText: 'Send a message...',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () {
+                    // Thêm logic gửi tin nhắn ở đây
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
