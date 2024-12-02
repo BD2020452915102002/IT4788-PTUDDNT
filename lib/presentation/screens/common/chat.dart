500904e25b4e746046547582669f9d3a948eb954
ducbd <duc.bd204529@sis.hust.edu.vn>
2024-12-02 21:42:54 +0700

    update ui

--- lib/presentation/screens/common/chat.dart
+++ lib/presentation/screens/common/chat.dart
@@ -1,5 +1,8 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:ptuddnt/core/utils/hive.dart';
+import 'package:ptuddnt/core/utils/token.dart';
+import 'package:stomp_dart_client/stomp_dart_client.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -9,51 +12,103 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  StompClient? stompClient;
+  List<Map<String, String>> messages = [];
+  final userId = HiveService().getData('userData')['id'];
+  final token = Token().get();
+  final _contentController = TextEditingController();
+
   @override
   void initState() {
     super.initState();
+    _connectWebSocket();
+  }
 
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Received message: ${message.notification?.title}, ${message.notification?.body}');
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('${message.notification?.title}: ${message.notification?.body}')),
-      );
+  void _connectWebSocket() {
+    print('duc111$userId  :   $token');
+    stompClient = StompClient(
+      config: StompConfig(
+        url: 'ws://157.66.24.126:8080/ws',
+        onConnect: _onConnect,
+        onWebSocketError: (error) => print('WebSocket error: $error'),
+      ),
+    );
+    stompClient!.activate();
+  }
+
+  void _onConnect(StompFrame frame) {
+    print('userId duc $userId');
+    stompClient!.subscribe(
+      destination: '/user/$userId/inbox',
+      callback: (frame) {
+        final body = jsonDecode(frame.body!);
+        print('body $body');
+        setState(() {
+          messages.add({
+            'sender': body['sender']['id'].toString(),
+            'content': body['content'],
+          });
+        });
+      },
+    );
+  }
+
+  void _sendMessage() {
+    final content = _contentController.text;
+    stompClient!.send(
+      destination: '/chat/message',
+      body: jsonEncode({
+        'receiver': {'id': 1}, // Change this as per requirement
+        'content': content,
+        'sender': userId,
+        'token': token,
+      }),
+    );
+    setState(() {
+      messages.add({'sender': 'You', 'content': content});
     });
+    _contentController.clear();
+  }
+
+  @override
+  void dispose() {
+    stompClient?.deactivate();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Chat'),
-      ),
+      appBar: AppBar(title: Text('Chat')),
       body: Column(
-        children: <Widget>[
+        children: [
           Expanded(
             child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (ctx, index) => ListTile(
-                title: Text('Message $index'),
-              ),
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                final message = messages[index];
+                final isSent = message['sender'] == 'You';
+                return Align(
+                  alignment: isSent ? Alignment.centerRight : Alignment.centerLeft,
+                  child: Container(
+                    margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: isSent ? Colors.blue[100] : Colors.grey[300],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Text(message['content']!),
+                  ),
+                );
+              },
             ),
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    decoration: const InputDecoration(
-                      labelText: 'Send a message...',
-                    ),
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.send),
-                  onPressed: () {
-                    // Thêm logic gửi tin nhắn ở đây
-                  },
-                ),
+              children: [
+                Expanded(child: TextField(controller: _contentController, decoration: InputDecoration(hintText: 'Type a message'))),
+                IconButton(icon: Icon(Icons.send), onPressed: _sendMessage),
               ],
             ),
           ),
